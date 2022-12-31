77c5f1a7434155cd622ff326f27397af05051729
Aleyna Dikal <aleyynadikal@gmail.com>
2022-12-31 22:58:43 +0300

    project 3

---
+++ project3/SQLQuery2.sql
@@ -0,0 +1,9 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [OgrID]
+      ,[Ad]
+      ,[Soyad]
+      ,[Cinsiyet]
+      ,[Proje]
+      ,[Vize]
+      ,[Final]
+  FROM [KAYNAK].[dbo].[ISE311SQL]
